782f32e91ec3d98a82f58c6c3a3933e8d8c577c9
jjfl1k <abdulatifrahmatullaev3@gmail.com>
2026-01-04 10:05:56 +0500

    Add files via upload
    Signed-off-by: jjfl1k <abdulatifrahmatullaev3@gmail.com>

--- index.js
+++ index.js
@@ -2,40 +2,14 @@
 const panel = document.getElementById('sidePanel');
 const showBtn = document.getElementById('showPanel');
 const closeBtn = document.getElementById('closePanel');
+const nbtn = document.getElementById('all');
 
-showBtn.addEventListener('click', () => {
-  panel.classList.add('active');
+nbtn.addEventListener('click',()=>{
+  panel.classList.add('active')
 });
-
 closeBtn.addEventListener('click', () => {
   panel.classList.remove('active');
 });
-
-
-
-
-showBtn.addEventListener('click', () => {
-  panel.classList.add('active');
-});
-
-closeBtn.addEventListener('click', () => {
-  panel.classList.remove('active');
-});
-
-const addButtons = document.querySelectorAll('.add-to-cart');
-
-addButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const product = button.closest('.product');
-    const name = product.dataset.name; 
-
-    
-    const li = document.createElement('li');
-    li.textContent = name;
-    cartItems.appendChild(li);
-
-    
-    panel.classList.add('active');
-  });
-});
-
+showBtn.addEventListener('click',()=>{
+  panel.classList.add('active')
+})
